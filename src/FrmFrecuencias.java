4ff2ef64dff6443b09f2e4433fb1e8c37c151b92
fray osorio <frayosorio@gmail.com>
2025-08-05 13:11:49 -0500

    Se agrega componente para listar las respuestas

--- src/FrmFrecuencias.java
+++ src/FrmFrecuencias.java
@@ -1,12 +1,16 @@
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
 import javax.swing.JTextArea;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 
 public class FrmFrecuencias extends JFrame {
 
     JComboBox cmbRespuesta;
+    JList lstRespuestas;
     String[] opciones = new String[] { "Excelente", "Buena", "Regular", "Mala" };
 
     public FrmFrecuencias() {
@@ -25,8 +29,25 @@ public class FrmFrecuencias extends JFrame {
 
         cmbRespuesta = new JComboBox();
         cmbRespuesta.setBounds(10, 60, 100, 25);
-        DefaultComboBoxModel mdlRespuesta=new DefaultComboBoxModel(opciones);
+        DefaultComboBoxModel mdlRespuesta = new DefaultComboBoxModel(opciones);
         cmbRespuesta.setModel(mdlRespuesta);
         getContentPane().add(cmbRespuesta);
+
+        JButton btnAgregar = new JButton("Agregar");
+        btnAgregar.setBounds(80, 90, 100, 25);
+        getContentPane().add(btnAgregar);
+
+        JButton btnQuitar = new JButton("Quitar");
+        btnQuitar.setBounds(80, 120, 100, 25);
+        getContentPane().add(btnQuitar);
+
+        lstRespuestas=new JList();
+        JScrollPane spRespuestas = new JScrollPane(lstRespuestas);
+        spRespuestas.setBounds(270, 10, 100, 150);
+        getContentPane().add(spRespuestas);
+
+        JButton btnCalcular = new JButton("Frecuencias");
+        btnCalcular.setBounds(10, 200, 150, 25);
+        getContentPane().add(btnCalcular);
     }
 }
